dfa708e1d45d6bf93f3aab2fee3dda132cfc19d9
saditrahman <saditsnigdho@gmail.com>
2021-07-02 16:45:43 +0600

    All source code added

--- app/src/main/java/com/example/gpsapplication/MainActivity.java
+++ app/src/main/java/com/example/gpsapplication/MainActivity.java
@@ -2,10 +2,14 @@ package com.example.gpsapplication;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
 import androidx.core.app.ActivityCompat;
 
 import android.Manifest;
+import android.annotation.SuppressLint;
 import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
 import android.location.Location;
 import android.os.Build;
 import android.os.Bundle;
@@ -15,9 +19,13 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
 import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
 import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.tasks.OnSuccessListener;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
     private static final int PERMISSIONS_FINE_LOCATION = 99;
@@ -26,6 +34,7 @@ public class MainActivity extends AppCompatActivity {
     Switch sw_locationupdates, sw_gps;
     boolean updateOn = false;
     LocationRequest locationRequest;
+    LocationCallback locationCallBack;
     FusedLocationProviderClient fusedLocationProviderClient;
 
     @Override
@@ -52,6 +61,16 @@ public class MainActivity extends AppCompatActivity {
         locationRequest.setFastestInterval(5000);
         locationRequest.setPriority(LocationRequest.PRIORITY_BALANCED_POWER_ACCURACY);
 
+        locationCallBack = new LocationCallback(){
+
+            @Override
+            public void onLocationResult(LocationResult locationResult) {
+                super.onLocationResult(locationResult);
+                //save the location
+                updateUIValues(locationResult.getLastLocation());
+            }
+        };
+
         sw_gps.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -66,25 +85,47 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        updateGPS();
-    } //end onCreate method
-
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-
-        switch (requestCode) {
-            case PERMISSIONS_FINE_LOCATION:
-                if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                    updateGPS();
+        sw_locationupdates.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(sw_locationupdates.isChecked()){
+                    //turn on location
+                    startLocationUpdates();
                 }
                 else {
-                    Toast.makeText(this, "This app requires permissions to be granted", Toast.LENGTH_SHORT).show();
-                    finish();
+                    //turn off location
+                    stopLocationUpdates();
                 }
-        }
+            }
+        });
+
+
+
+        updateGPS();
+
+    } //end onCreate method
+
+
+    private void stopLocationUpdates() {
+        tv_updates.setText("Location is NOT tracked");
+        tv_lat.setText("Not tracking location");
+        tv_lon.setText("Not tracking location");
+        tv_speed.setText("Not tracking location");
+        tv_accuracy.setText("Not tracking location");
+        tv_altitude.setText("Not tracking location");
+        tv_address.setText("Not tracking location");
+        tv_sensor.setText("Not tracking location");
+
+        fusedLocationProviderClient.removeLocationUpdates(locationCallBack);
     }
 
+    @SuppressLint("MissingPermission")
+    private void startLocationUpdates() {
+
+        tv_updates.setText("Location is being tracked");
+        fusedLocationProviderClient.requestLocationUpdates(locationRequest, locationCallBack, null);
+        updateGPS();
+    }
 
     //Periodically updates GPS
     private void updateGPS() {
@@ -129,6 +170,16 @@ public class MainActivity extends AppCompatActivity {
             tv_speed.setText(String.valueOf(location.getSpeed()));
         } else {
             tv_speed.setText("Not available");
+        }
+
+        Geocoder geocoder = new Geocoder(MainActivity.this);
+
+        try {
+            List<Address> addresses = geocoder.getFromLocation(location.getLatitude(), location.getLongitude(), 1);
+            tv_address.setText(addresses.get(0).getAddressLine(0));
+        }
+        catch (Exception e) {
+            tv_address.setText("Unable to get street address");
         }
     }
 }
